53acdb599f0a3f6b201528c17f121a0f2a837e25
xcatliu <xcatliu@gmail.com>
2016-02-06 10:16:24 +0800

    Add gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,78 @@
+/* eslint no-console:0 */
+
 const gulp = require('gulp');
-const rimraf = require('rimraf');
 
-
+/* clean */
 gulp.task('clean', ['clean:gh-pages', 'clean:lib']);
 
-gulp.task('clean:gh-pages', (cb) => {
-  rimraf('./gh-pages', cb);
+gulp.task('clean:gh-pages', () => {
+  const rimraf = require('rimraf');
+  rimraf.sync('./gh-pages');
 });
 
-gulp.task('clean:lib', (cb) => {
-  rimraf('./lib', cb);
+gulp.task('clean:lib', () => {
+  const rimraf = require('rimraf');
+  rimraf.sync('./lib');
 });
+
+/* build lib */
+gulp.task('build:lib', ['clean:lib', 'build:lib:copy', 'build:lib:babel']);
+
+gulp.task('build:lib:copy', () => {
+  gulp.src('./src/**/*.json')
+    .pipe(gulp.dest('./lib'));
+});
+
+gulp.task('build:lib:babel', () => {
+  const babel = require('gulp-babel');
+  gulp.src('./src/index.js')
+    .pipe(babel())
+    .pipe(gulp.dest('./lib'));
+});
+
+/* build site */
+gulp.task('build:site', [
+  'clean:gh-pages',
+  'build:site:copy',
+  'build:site:sass',
+  'build:site:index',
+]);
+
+gulp.task('build:site:copy', () => {
+  gulp.src('./src/**/*.json').pipe(gulp.dest('./gh-pages'));
+});
+
+gulp.task('build:site:sass', () => {
+  const sass = require('gulp-sass');
+
+  gulp.src('./site/sass/index.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./gh-pages/assets'));
+});
+
+gulp.task('build:site:index', () => {
+  const swig = require('gulp-swig');
+  const data = require('./lib');
+
+  gulp.src('./site/index.html')
+    .pipe(swig({ data }))
+    .pipe(gulp.dest('./gh-pages'));
+});
+
+/* serve */
+gulp.task('serve', () => {
+  const http = require('http');
+  const ecstatic = require('ecstatic');
+
+  http.createServer(
+    ecstatic({
+      root: __dirname + '/gh-pages',
+      cache: 0,
+      showDir: true,
+    })
+  ).listen(8080);
+
+  console.log('ecstatic serving gh-pages at http://0.0.0.0:8080');
+});
+
+gulp.task('prepublish', ['clean:lib', 'build:lib']);
